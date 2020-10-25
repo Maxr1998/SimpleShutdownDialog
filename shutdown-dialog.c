4ae912c6474b884455f3ae1384062e12785347ca
timm0e <11885527+timm0e@users.noreply.github.com>
2020-10-25 14:30:20 +0100

    :sparkles: add hotkeys for all of the buttons

--- shutdown-dialog.c
+++ shutdown-dialog.c
@@ -60,10 +60,36 @@ void on_window_show(GtkWidget *w, gpointer data) {
 }
 
 gboolean on_key(GtkWidget *widget, GdkEventKey *event, gpointer data) {
-    if (event->keyval == GDK_KEY_Escape || event->keyval == GDK_KEY_q) {
+    if (event->keyval == GDK_KEY_Escape || event->keyval == EXIT_HOTKEY) {
         gtk_main_quit();
         return TRUE;
     }
+
+    // handle commands
+
+    char *cmd = NULL;
+
+    switch (event->keyval) {
+            case SHUTDOWN_HOTKEY:
+                    cmd = SHUTDOWN_CMD;
+                    break;
+            case REBOOT_HOTKEY:
+                    cmd = REBOOT_CMD;
+                    break;
+            case SUSPEND_HOTKEY:
+                    cmd = SUSPEND_CMD;
+                    break;
+            case HIBERNATE_HOTKEY:
+                    cmd = HIBERNATE_CMD;
+                    break;
+    }
+
+    if (cmd) {
+        system(cmd);
+	gtk_main_quit();
+        return TRUE;
+    }
+
     return FALSE;
 }
 
